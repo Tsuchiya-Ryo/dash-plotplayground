d4938707dfe27590614c1d08a7eeaff5c5e965db
Tsuchiya-Ryo <66044977+Tsuchiya-Ryo@users.noreply.github.com>
2020-10-31 22:55:34 +0900

    color palette

--- app.py
+++ app.py
@@ -1,29 +1,65 @@
 import base64, io
+import pandas as pd
+import numpy as np
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import pandas as pd
 import plotly.graph_objs as go
-from sklearn.preprocessing import LabelEncoder
-import numpy as np
 
+from sklearn.preprocessing import LabelEncoder
 
 app = dash.Dash(__name__)
 
+# STYLE PARAMETER SETTINGS ----------------------------------------------------------
+colorpalette = [{"label":x, "value":x} for x in [
+                "Viridis", "Cividis", "Inferno", "Electric", "Rainbow",
+                "Plasma", "Speed","Jet", "Plotly3", "Bluered","Thermal",
+                "Portland", "HSV", "Phase", "Mrybm", "Mygbm"]]
+filetypes = [{"label":"[tab separated .txt]", "value":"txt"},
+            {"label":"[comma separated .csv]", "value":"csv"},
+            {"label":"[excel file .xlsx/.xls]", "value":"xlsx"}]
+filelimit = 750000
+uploadstyle = {'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'}
+opacityset = [{"label": round(i, 1), "value": round(i, 1)}\
+            for i in np.linspace(0.1, 1, 10) ]
+marksize = [{"label":round(i,0), "value":round(i,0)}\
+            for i in np.linspace(3,16,14)]
+dropdownbox = {"height":"30px", "width":"150px", "display":"inline-block"}
+axis_name_input = {"height":"25px","width":"200px", "borderRadius":"5px",
+                    "font-size":"16px", "borderWidth":"1px"}
+multisheetscount = [{"label": str(int(i)), "value": int(i)} for i in np.linspace(1, 5, 5)]
+xy_axis_position_2d = [{"label":i, "value":j} for i, j in zip(["1st","2nd","3rd"],range(3))]
+xyz_axis_position_3d = [{"label":i, "value":j} for i, j in zip(["1st","2nd","3rd","4th"],range(4))]
+label_axis_position_2d = xy_axis_position_2d.copy()
+label_axis_position_2d.append({"label":"None","value":"NONE"})
+label_axis_position_3d = xyz_axis_position_3d.copy()
+label_axis_position_3d.append({"label":"None","value":"NONE"})
+
+# INTERFACE --------------------------------------------------------------------------
+# HEADER -----------------------------------------------------------------------------
 app.layout = html.Div(children=[
     html.H1(children="Plot Playground"),
+    html.Div([
     html.A("HOME", href="https://tsuchiya-ryo.github.io/orgsynscalc/"),
-    html.H2(children="2d-plot"),
+    html.Br(),
+    html.A("Plot使い方", href="https://tsuchiya-ryo.github.io/orgsynscalc/plot-explanation.html")
+    ]),
 
+# 2D PLOT AREA -----------------------------------------------------------------------
+    html.H2(children="2d-plot"),
     html.Div([
         html.Div(children="input file type:", style={"display":"inline-block"}),
         dcc.RadioItems(id="filetype2d",
-            options=[
-                {"label":"[tab separated .txt]", "value":"txt"},
-                {"label":"[comma separated .csv]", "value":"csv"},
-                {"label":"[.xlsx or .xls]", "value":"xlsx"}
-            ],
+            options=filetypes,
             value="txt",
             style={"display":"inline-block"}
         )
@@ -32,7 +68,7 @@ app.layout = html.Div(children=[
     html.Div([
     html.Div(children="multiple sheets (for .xlsx/.xls, columns must be the same position):", style={"display":"inline-block"}),
     dcc.RadioItems(id="multisheets2d",
-                    options=[{"label": str(int(i)), "value": int(i)} for i in np.linspace(1, 5, 5)],
+                    options=multisheetscount,
                     value = 1,
                     style={"display":"inline-block"})
     ]),
@@ -41,12 +77,7 @@ app.layout = html.Div(children=[
     html.Div([
         html.Div(children="Label :", style={"display":"inline-block"}),
         dcc.RadioItems(id="labelaxis2d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2},
-                {"label":"None", "value":"NONE"}
-            ],
+            options=label_axis_position_2d,
             value="NONE",
             style={"display":"inline-block"}
         )
@@ -54,11 +85,7 @@ app.layout = html.Div(children=[
     html.Div([
         html.Div(children="x-axis:", style={"display":"inline-block"}),
         dcc.RadioItems(id="xaxis2d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2}
-            ],
+            options=xy_axis_position_2d,
             value=0,
             style={"display":"inline-block"}
         )
@@ -66,93 +93,57 @@ app.layout = html.Div(children=[
         html.Div([
         html.Div(children="y-axis:", style={"display":"inline-block"}),
         dcc.RadioItems(id="yaxis2d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2}
-            ],
+            options=xy_axis_position_2d,
             value=1,
             style={"display":"inline-block"}
-        )
-    ]),
+        )]),
 
-
-   
     html.Br(),
+
     dcc.Upload(
         id='upload-data',
         children=html.A("Click to Select 2d File (or drag and drop)"),
         multiple=True,
-        max_size = 750000,
-     style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-     }),
+        max_size=filelimit,
+        style=uploadstyle),
     html.Br(),
 
     html.Div([
         dcc.Dropdown(
         id="colorpalette2d",
-        options=[
-            {"label":"Viridis", "value":"viridis"},
-            {"label":"Inferno", "value":"inferno"},
-            {"label":"Twilight", "value":"twilight"},
-            {"label":"Earth", "value":"earth"},
-            {"label":"Tropic", "value":"tropic"},
-            {"label":"Icefire","value":"icefire"},
-            {"label":"Greys", "value":"greys"},
-            {"label":"Electric", "value":"electric"},
-            {"label":"Rainbow", "value":"rainbow"},
-            {"label":"Plasma", "value":"plasma"},
-            {"label":"Picnic", "value":"picnic"},
-            {"label":"Speed", "value":"speed"},
-            {"label":"Balance", "value":"balance"}
-        ],
-        value="viridis",
-        style={"height":"30px", "width":"150px", "display":"inline-block"}
+        options=colorpalette,
+        value="Viridis",
+        style=dropdownbox
     ),
     dcc.Dropdown(
         id="opacity2d",
-        options=[{"label": round(i, 1), "value": round(i, 1)}\
-             for i in np.linspace(0.1, 1, 10) ],
-             value=0.5,
-             style={"height":"30px", "width":"150px", "display":"inline-block"}
+        options=opacityset,
+        value=0.5,
+        style=dropdownbox
     ),
     dcc.Dropdown(
         id="size2d",
-        options=[{"label":round(i,0), "value":round(i,0)}\
-            for i in np.linspace(3,12,10)],
-            value=5,
-            style={"height":"30px", "width":"150px",  "display":"inline-block"}
+        options=marksize,
+        value=5,
+        style=dropdownbox
     )]),
     html.Br(),
 
     html.P(id="title2d"),
-    dcc.Input(id="xlabel", type="text", value="x"),
-    dcc.Input(id="ylabel", type="text", value="y"),
+    dcc.Input(id="xlabel", type="text", value="x", style=axis_name_input),
+    dcc.Input(id="ylabel", type="text", value="y", style=axis_name_input),
 
     html.Div([
         html.Div([dcc.Graph(id="example-graph")],
         style={"height":"100%", "width":"100%"})
     ]),
-
-
+# 3D PLOT AREA --------------------------------------------------------------------
     html.H2(children="3d-plot"),
 
      html.Div([
         html.Div(children="input file type:", style={"display":"inline-block"}),
         dcc.RadioItems(id="filetype3d",
-            options=[
-                {"label":"[tab separated .txt]", "value":"txt"},
-                {"label":"[comma separated .csv]", "value":"csv"},
-                {"label":"[.xlsx or .xls]", "value":"xlsx"}
-            ],
+            options=filetypes,
             value="txt",
             style={"display":"inline-block"}
         )
@@ -161,7 +152,7 @@ app.layout = html.Div(children=[
     html.Div([
     html.Div(children="multiple sheets (for .xlsx/.xls, columns must be the same position):", style={"display":"inline-block"}),
     dcc.RadioItems(id="multisheets3d",
-                    options=[{"label": str(int(i)), "value": int(i)} for i in np.linspace(1, 5, 5)],
+                    options=multisheetscount,
                     value = 1,
                     style={"display":"inline-block"})
     ]),
@@ -170,119 +161,68 @@ app.layout = html.Div(children=[
     html.Div([
         html.Div(children="Label :", style={"display":"inline-block"}),
         dcc.RadioItems(id="labelaxis3d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2},
-                {"label":"4th", "value":3},
-                {"label":"None", "value":"NONE"}
-            ],
+            options=label_axis_position_3d,
             value="NONE",
             style={"display":"inline-block"}
-        )
-    ]),
+        )]),
     html.Div([
         html.Div(children="x-axis:", style={"display":"inline-block"}),
         dcc.RadioItems(id="xaxis3d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2},
-                {"label":"4th", "value":3}
-            ],
+            options=xyz_axis_position_3d,
             value=0,
             style={"display":"inline-block"}
-        )
-    ]),
+        )]),
         html.Div([
         html.Div(children="y-axis:", style={"display":"inline-block"}),
         dcc.RadioItems(id="yaxis3d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2},
-                {"label":"4th", "value":3}
-            ],
+            options=xyz_axis_position_3d,
             value=1,
             style={"display":"inline-block"}
-        )
-    ]),
+        )]),
         html.Div([
         html.Div(children="z-axis:", style={"display":"inline-block"}),
         dcc.RadioItems(id="zaxis3d",
-            options=[
-                {"label":"1st", "value":0},
-                {"label":"2nd", "value":1},
-                {"label":"3rd", "value":2},
-                {"label":"4th", "value":3}
-            ],
+            options=xyz_axis_position_3d,
             value=2,
             style={"display":"inline-block"}
-        )
-    ]),
-
+        )]),
 
     html.Br(),
+
     dcc.Upload(
         id='upload-data3d',
         children=html.A("Click to Select 3d File (or drag and drop)"),
         multiple=True,
-        max_size = 750000,
-
-     style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-     }),
+        max_size=filelimit,
+        style=uploadstyle),
      html.Br(),
 
     html.Div([
     dcc.Dropdown(
         id="colorpalette3d",
-        options=[
-            {"label":"Viridis", "value":"viridis"},
-            {"label":"Inferno", "value":"inferno"},
-            {"label":"Twilight", "value":"twilight"},
-            {"label":"Earth", "value":"earth"},
-            {"label":"Tropic", "value":"tropic"},
-            {"label":"Icefire","value":"icefire"},
-            {"label":"Greys", "value":"greys"},
-            {"label":"Electric", "value":"electric"},
-            {"label":"Rainbow", "value":"rainbow"},
-            {"label":"Plasma", "value":"plasma"},
-            {"label":"Picnic", "value":"picnic"},
-            {"label":"Speed", "value":"speed"},
-            {"label":"Balance", "value":"balance"}
-        ],
-        value="viridis",
-        style={"height":"30px", "width":"150px", "display":"inline-block"}
+        options=colorpalette,
+        value="Viridis",
+        style=dropdownbox
     ),
     dcc.Dropdown(
         id="opacity3d",
-        options=[{"label": round(i, 1), "value": round(i, 1)}\
-             for i in np.linspace(0.1, 1, 10) ],
-             value=0.5,
-             style={"height":"30px", "width":"150px", "display":"inline-block"}
+        options=opacityset,
+        value=0.5,
+        style=dropdownbox
     ),
     dcc.Dropdown(
         id="size3d",
-        options=[{"label":round(i,0), "value":round(i,0)}\
-            for i in np.linspace(3,12,10)],
-            value=5,
-            style={"height":"30px", "width":"150px", "display":"inline-block"}
+        options=marksize,
+        value=5,
+        style=dropdownbox
     )]),
+
     html.Br(),
 
-
     html.P(id="title3d"),
-    dcc.Input(id="xlabel3d", type="text", value="x"),
-    dcc.Input(id="ylabel3d", type="text", value="y"),
-    dcc.Input(id="zlabel3d", type="text", value="z"),
+    dcc.Input(id="xlabel3d", type="text", value="x", style=axis_name_input),
+    dcc.Input(id="ylabel3d", type="text", value="y", style=axis_name_input),
+    dcc.Input(id="zlabel3d", type="text", value="z", style=axis_name_input),
     html.Div([
         html.Div([dcc.Graph(id="example-graph3d")],
         style={"height":"100%", "width":"100%"})
@@ -291,8 +231,10 @@ app.layout = html.Div(children=[
     html.Br(),
     html.Br(),
     html.Br()
-])
+    ])
+# INTERFACE END -----------------------------------------------------------------------
 
+# READ FILE AND RETURN DATAFRAME 2D-----------------------------------------------------
 def parse_contents(contents, filetype, numsheets, xaxis, yaxis, labelaxis):
     _, content_string = contents.split(",")
     decoded = base64.b64decode(content_string)
@@ -315,8 +257,7 @@ def parse_contents(contents, filetype, numsheets, xaxis, yaxis, labelaxis):
 
     return df
 
-##------------------------------------------------
-
+# READ FILE AND RETURN DATAFRAME 3D-----------------------------------------------------
 def parse_contents3d(contents3d, filetype, numsheets, xaxis, yaxis, zaxis, labelaxis):
     _, content_string3d = contents3d.split(",")
     decoded3d = base64.b64decode(content_string3d)
@@ -340,14 +281,14 @@ def parse_contents3d(contents3d, filetype, numsheets, xaxis, yaxis, zaxis, label
     return df3d
  
 
-# callback for rendering 2d graph
+# CALLBACK FOR 2D GRAPH ------------------------------------------------------------
 @app.callback(
     Output('example-graph', 'figure'),
 
-    Input('upload-data', 'contents'), # contents
-    Input("filetype2d", "value"), # filetype
-    Input("multisheets2d", "value"), # numsheets
-    Input("xaxis2d", "value"), # xaxis
+    Input('upload-data', 'contents'),
+    Input("filetype2d", "value"),
+    Input("multisheets2d", "value"),
+    Input("xaxis2d", "value"),
     Input("yaxis2d", "value"),
     Input("labelaxis2d", "value"),
     Input("xlabel", "value"),
@@ -397,7 +338,7 @@ def update_graph(contents, filetype, numsheets, xaxis, yaxis, labelaxis, xcolumn
         yaxis={"title":ycolumn}
     )}
 
-# callback for showing inputted 3d filename
+# CALLBACK FOR DISPLAY FILENAME 2D--------------------------------------------------
 @app.callback(
     Output("title2d", "children"),
     Input("upload-data", "filename")
@@ -405,7 +346,7 @@ def update_graph(contents, filetype, numsheets, xaxis, yaxis, labelaxis, xcolumn
 def show_filename(filename):
     return filename
 
-# callback for rendering 3d graph
+# CALLBACK FOR 3D GRAPH ------------------------------------------------------------
 @app.callback(
     Output("example-graph3d", "figure"),
 
@@ -472,7 +413,7 @@ def update_graph3d(contents3d, filetype, numsheets, xaxis, yaxis, zaxis, labelax
     )
      }
 
-# callback for showing inputted 3d filename
+# CALLBACK FOR DISPLAY FILENAME 3D ----------------------------------------------------
 @app.callback(
     Output("title3d", "children"),
     Input("upload-data3d", "filename")
@@ -480,6 +421,6 @@ def update_graph3d(contents3d, filetype, numsheets, xaxis, yaxis, zaxis, labelax
 def show_filename3d(filename):
     return filename
 
-
+# RUN APPLICATION ---------------------------------------------------------------------
 if __name__ == '__main__':
     app.run_server(host="0.0.0.0")
